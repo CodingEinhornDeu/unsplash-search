f2822cb7ebfaed23201dd781e906a676fb1ab354
CodingEinhornDeu <88826323+CodingEinhornDeu@users.noreply.github.com>
2022-01-28 21:56:56 +0100

    logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Search from './Search';
-import logos from './logo.svg';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-        <img className="logo" src={logos}></img>
+  <div className="App">
+      <img className="logo" src={logo}></img>
       <Search/>
-    </div>
+  </div>
   );
 }
 
